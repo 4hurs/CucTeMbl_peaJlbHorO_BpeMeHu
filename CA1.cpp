a5a919b659b72182ebc31e88afcc746b4468342b
4hurs <fawstainer@gmail.com>
2025-12-12 00:57:45 +0200

    .

---
+++ CA1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <thread>
+#include <string>
+#include <time.h>
+
+using namespace std;
+
+int factorial(int n) {
+    int result = 1;
+    for (int i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+void Func(string name) {
+    const int iterations = 10000000; 
+    const int number = 10;          
+
+    long long total = 0;
+    for (int i = 0; i < iterations; i++) {
+        total += factorial(number);
+    }
+}
+
+int main() {
+    clock_t start_parallel = clock();
+
+    std::thread thread1(Func, "t1");
+    std::thread thread2(Func, "t2");
+
+    thread1.join();
+    thread2.join();
+
+    clock_t end_parallel = clock();
+    double parallel_time = (double)(end_parallel - start_parallel) / CLOCKS_PER_SEC;
+    cout << "Parallel time: " << parallel_time << " seconds" << endl << endl;
+    //
+    //
+    //
+    clock_t start_sequential = clock();
+
+    Func("sequential1");
+    Func("sequential2");
+
+    clock_t end_sequential = clock();
+    double sequential_time = (double)(end_sequential - start_sequential) / CLOCKS_PER_SEC;
+    cout << "Sequential time: " << sequential_time << " seconds" << endl << endl;
+
+    system("pause");
+    return 0;
+}
---
+++ CA2.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <string>
+#include <chrono>
+
+//std::mutex m;
+
+void Func(std::string name) {
+    //long double для точности
+    long double i = 0.0;
+    const long double increment = 1e-9; 
+
+    auto start_time = std::chrono::high_resolution_clock::now();
+
+    //цикл на 1 сек
+    while (std::chrono::high_resolution_clock::now() - start_time < std::chrono::seconds(1)) {
+        i += increment;
+    }
+
+    //блок мьютекса m.lock();
+    std::cout << "Thread " << name << " finished with i = " << i << std::endl;
+    //разблок мьютекса m.unlock();
+}
+
+int main() {
+    std::cout.precision(10);
+
+    std::thread thread1(Func, "t1");
+    std::thread thread2(Func, "t2");
+    std::thread thread3(Func, "t3");
+
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    return 0;
+}
